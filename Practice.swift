b14110c24f02069a477c7b45eb512a66d6c3e83c
addi <addisonkenefick@gmail.com>
2025-12-01 08:22:57 -0600

    i dont know

---
+++ Practice.swift
@@ -0,0 +1,8 @@
+//
+//  Practice.swift
+//  StatTrackKenefick
+//
+//  Created by ADDISON KENEFICK on 11/20/25.
+//
+
+import Foundation
---
+++ PracticeView.swift
@@ -0,0 +1,18 @@
+//
+//  PracticeView.swift
+//  StatTrackKenefick
+//
+//  Created by ADDISON KENEFICK on 11/14/25.
+//
+
+import SwiftUI
+
+struct PracticeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PracticeView()
+}
